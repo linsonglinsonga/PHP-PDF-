1486c971455e9d5a0cf459ef8f89dcd86dbc4bfb
Ts <G6728_27@163.com>
2018-10-17 14:55:56 +0800

    Add files via upload

---
+++ word.php
@@ -0,0 +1,47 @@
+<?php
+
+require_once('./fpdf/fpdf.php');
+require_once('./fpdi/fpdi.php');
+
+
+//word_watermark
+$pdf = new FPDI();
+// 设置字体
+$pdf->AddGBFont('Arial', '宋体');
+
+// get the page count
+$pageCount = $pdf->setSourceFile('terms.pdf');
+
+// iterate through all pages
+for ($pageNo = 1; $pageNo <= $pageCount; $pageNo++)
+{
+
+    // import a page
+    $templateId = $pdf->importPage($pageNo);
+
+    // get the size of the imported page
+    $size = $pdf->getTemplateSize($templateId);
+
+    // create a page (landscape or portrait depending on the imported page size)
+    if ($size['w'] > $size['h']) $pdf->AddPage('L', array($size['w'], $size['h']));
+    else $pdf->AddPage('P', array($size['w'], $size['h']));
+
+    // use the imported page
+    $pdf->useTemplate($templateId);
+
+    if ($pageNo == 1) {
+
+        $pdf->SetFont('Arial', '', 10);
+        $pdf->SetXY(50, 70);
+        $pdf->Write (7, iconv("utf-8","gbk","黄渤"));
+        $pdf->SetXY(61, 78.5);
+        $pdf->Write(7, '123456789876543212');
+        $pdf->SetXY(58, 85.5);
+        $pdf->Write(7, '13121462911');
+
+    }
+
+}
+$pdf->Output(date('Y-m-d H-i-s').'word.pdf');
+
+
